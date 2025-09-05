5973381e2a34c93b4502101b0aebf8d54e9047d5
Sally <sally.rdz.vargas.97@gmail.com>
2025-09-04 18:40:17 -0600

    se migro a astro la pagina web

--- script/index.js
+++
@@ -1,6 +0,0 @@
-// menu 
-const btnMenu = document.getElementById("btn-menu")
-btnMenu.addEventListener("click", ()=>{
-    const menu = document.getElementById("menu");
-    menu.style.display = menu.style.display==="flex"? "none" : "flex"
-})
